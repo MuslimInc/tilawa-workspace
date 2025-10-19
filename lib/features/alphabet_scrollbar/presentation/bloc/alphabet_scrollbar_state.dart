6dcd3de9c9f2ce78ee80ad26b380363817bb111d
Muhammad Kamel <mu7ammadkamel@hotmail.com>
2025-10-19 03:45:27 +0300

    refactor: migrate AlphabetScrollbarBloc and AlphabetScrollbarState to use Freezed for improved state management and simplify event handling

--- lib/features/alphabet_scrollbar/presentation/bloc/alphabet_scrollbar_state.dart
+++ lib/features/alphabet_scrollbar/presentation/bloc/alphabet_scrollbar_state.dart
@@ -1,29 +1,9 @@
 part of 'alphabet_scrollbar_bloc.dart';
 
-abstract class AlphabetScrollbarState extends Equatable {
-  const AlphabetScrollbarState();
-
-  @override
-  List<Object?> get props => [];
-}
-
-class AlphabetScrollbarInitial extends AlphabetScrollbarState {
-  const AlphabetScrollbarInitial();
-}
-
-class AlphabetScrollbarLoaded extends AlphabetScrollbarState {
-  final String? selectedLetter;
-  final bool isDragging;
-
-  const AlphabetScrollbarLoaded({this.selectedLetter, this.isDragging = false});
-
-  @override
-  List<Object?> get props => [selectedLetter, isDragging];
-
-  AlphabetScrollbarLoaded copyWith({String? selectedLetter, bool? isDragging}) {
-    return AlphabetScrollbarLoaded(
-      selectedLetter: selectedLetter ?? this.selectedLetter,
-      isDragging: isDragging ?? this.isDragging,
-    );
-  }
+@freezed
+abstract class AlphabetScrollbarState with _$AlphabetScrollbarState {
+  const factory AlphabetScrollbarState({
+    String? selectedLetter,
+    @Default(false) bool isDragging,
+  }) = _AlphabetScrollbarState;
 }
